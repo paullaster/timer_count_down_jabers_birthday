1557f69eb89207f3a22455d8085874742cc551fe
paullaster <paullasterokoth98@gmail.com>
2022-09-29 14:32:23 +0300

    getting second

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const countDown = (bd) => {
     const birthday = Date.parse (bd);
     const days = Math.floor (birthday / (24 * 60 * 60 * 1000));
     const hours = Math.floor ((birthday / (60 * 60 * 1000)) % 24);
-    const minutes = Math.floor ((birthday / 1000 / 60) %  60 )
+    const minutes = Math.floor ((birthday / 1000 / 60) %  60 );
+    const seconds = Math.floor ((birthday / 1000 ) % 60 );
     //const milliseconds = birthday / 1000;
-    console.log (days, hours, minutes);
+    console.log (days, hours, minutes, seconds);
 };
 
 countDown(babyBirthDay);
