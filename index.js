0e9c2b014269c9a35ff3f1173b3e47b464968563
paullaster <paullasterokoth98@gmail.com>
2022-09-29 22:32:07 +0300

    setting timer

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const babyBirthDay = 'Apr, 22 2023';
 
 //count down
 const countDown = (bd) => {
-    const birthday = Date.parse (bd);
+    const birthday = Date.parse (bd) - Date.now ();
     const days = Math.floor (birthday / (24 * 60 * 60 * 1000));
     const hours = Math.floor ((birthday / (60 * 60 * 1000)) % 24);
     const minutes = Math.floor ((birthday / 1000 / 60) %  60 );
@@ -21,7 +21,15 @@ const countDown = (bd) => {
 
 const update = (cb, dt) => {
     let temp = cb (dt);
-    console.log (temp);
+
+    let countingOutput = '';
+    for ( let prop in temp){
+        countingOutput += ` ${temp[prop]} : ${prop}  `;
+    }
+    console.log (countingOutput);
+    setTimeout ( () => {
+        update (countDown, babyBirthDay);
+    }, 1000);
 };
 
 update (countDown, babyBirthDay);
