6e87e70a54c58e71f9a8002ec9bb341df71d6a72
paullaster <paullasterokoth98@gmail.com>
2022-09-29 14:35:06 +0300

    count down

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ const countDown = (bd) => {
     const hours = Math.floor ((birthday / (60 * 60 * 1000)) % 24);
     const minutes = Math.floor ((birthday / 1000 / 60) %  60 );
     const seconds = Math.floor ((birthday / 1000 ) % 60 );
-    //const milliseconds = birthday / 1000;
-    console.log (days, hours, minutes, seconds);
+    return {
+        days,
+        hours,
+        minutes,
+        seconds,
+    }
 };
 
 countDown(babyBirthDay);
