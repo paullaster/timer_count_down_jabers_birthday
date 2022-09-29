6183ba050c2f5f7f8322c2e74ae1bbc4bf1ff89f
paullaster <paullasterokoth98@gmail.com>
2022-09-29 14:30:45 +0300

    get time

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const babyBirthDay = 'Apr, 22 2023';
 const countDown = (bd) => {
     const birthday = Date.parse (bd);
     const days = Math.floor (birthday / (24 * 60 * 60 * 1000));
-    const hours = Math.floor ((birthday / (260 * 60 * 1000)) % 24);
+    const hours = Math.floor ((birthday / (60 * 60 * 1000)) % 24);
+    const minutes = Math.floor ((birthday / 1000 / 60) %  60 )
     //const milliseconds = birthday / 1000;
-    console.log (days, hours);
+    console.log (days, hours, minutes);
 };
 
 countDown(babyBirthDay);
