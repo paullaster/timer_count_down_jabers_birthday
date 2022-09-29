f30d9c10e79bc592146c41107c2a21723e1d0647
paullaster <paullasterokoth98@gmail.com>
2022-09-29 14:27:10 +0300

    getting days from parsed date

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const babyBirthDay = 'Apr, 22 2023';
 //count down
 const countDown = (bd) => {
     const birthday = Date.parse (bd);
-    const year = birthday.getYears ();
-    console.log (year);
+    const days = Math.floor (birthday / (24 * 60 * 60 * 1000));
+    const hours = Math.floor ((birthday / (260 * 60 * 1000)) % 24);
+    //const milliseconds = birthday / 1000;
+    console.log (days, hours);
 };
 
 countDown(babyBirthDay);
