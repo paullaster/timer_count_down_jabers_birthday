f95427cba5dec8a07422f91a21cc55fc0acb56d2
paullaster <paullasterokoth98@gmail.com>
2023-01-05 00:03:02 +0300

    add

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * Count down timer to Baby
  * Synthia's birthday.
  */
-const babyBirthDay = 'Apr, 22 2023';
+const babyBirthDay = 'Apr, 22 2021';
 
 //count down
 const countDown = (bd) => {
-    const birthday = Date.parse (bd) - Date.now ();
+    const birthday =  Date.now () - Date.parse (bd);
     const days = Math.floor (birthday / (24 * 60 * 60 * 1000));
     const hours = Math.floor ((birthday / (60 * 60 * 1000)) % 24);
     const minutes = Math.floor ((birthday / 1000 / 60) %  60 );
@@ -26,10 +26,10 @@ const update = (cb, dt) => {
     for ( let prop in temp){
         countingOutput += ` ${temp[prop]} : ${prop}  `;
     }
-    console.log (countingOutput);
     setTimeout ( () => {
         update (countDown, babyBirthDay);
     }, 1000);
+    console.log (countingOutput);
 };
 
 update (countDown, babyBirthDay);
